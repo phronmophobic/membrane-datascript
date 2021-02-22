ceac4257420a82874f370c057000ebf6f6424f54
Adrian Smith <smith.adriane@gmail.com>
2021-02-22 00:19:52 -0800

    removing unnecessary comments.

--- src/com/phronemophobic/membrane_datascript.clj
+++ src/com/phronemophobic/membrane_datascript.clj
@@ -18,7 +18,6 @@
              ;;:todo/project {:db/valueType :db.type/ref}
              :complete?    {:db/index true}
              :todo {:db/valueType :db.type/ref}
-             ;; :todo/due     {:db/index true}
              :todo-lists {:db/cardinality :db.cardinality/many
                           :db/valueType :db.type/ref}
              :todos {:db/cardinality :db.cardinality/many
@@ -28,54 +27,10 @@
 ;; create connection to DB with schema
 (def conn (d/create-conn schema))
 
-(comment
-  (d/transact! conn
-               [
-                {:db/id 1
-                 
-                 :todo-lists [{:todos [{:complete? false
-                                        :description "Asdfdasf"}
-                                       {:complete? false
-                                        :description "Asdfdasf 2"}
-                                       {:complete? true
-                                        :description "Asdfdasf 3"}]}
-                              {:todos [{:complete? false
-                                        :description "aaa"}
-                                       {:complete? false
-                                        :description "aa 2"}
-                                       {:complete? true
-                                        :description "aaa 3"}]}
-                              {:todos [{:complete? false
-                                        :description "bb"}
-                                       {:complete? false
-                                        :description "bb 2"}
-                                       {:complete? true
-                                        :description "bb 3"}]}]}])
-  
-
-  (d/transact! conn
-               [
-                
-                {:complete? false
-                 :description "Asdfds"}])
-  ,)
-
-
-
-(comment
-  (d/transact! conn [[:db/retract 1 :todo-list/todos 8] ]))
-
 (defn e-by-av [db a v]
   (-> (d/datoms db :avet a v) first :e))
 
 
-(comment
-  (d/q '[:find ?m
-         :where
-         [?e]
-         [(pull ?e :done :text) ?m]]
-       @conn)
- )
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
@@ -86,74 +41,6 @@
    ui/vertical-layout
    (for [todos todo-lists]
      (td/todo-app :todos (:todos todos)))))
-
-;; (def todo-state (atom {:todos []}))
-
-(defn update-db [db schema path eid attr f]
-  (cond
-    (vector? path)
-    (mapv (partial path->datalog schema) path)
-
-    (seq? path)
-    (let [arg (second path)]
-      (case (first path)
-        nth
-        (spec/nthpath arg)
-        
-        get
-        (if attr
-          (d/with db
-                  [{:db/id eid
-                    attr (f (get (d/entity db eid) attr))}])
-          (d/with db [{}]))
-        ;; (spec/keypath arg)
-
-        ;; path
-        ;; (spec/path arg)
-
-        ;; raw-path
-        ;; (spec/path (path->datalog arg))
-
-        keypath
-        (spec/keypath arg)
-
-        keypath-list
-        (apply spec/keypath arg)
-
-        filter
-        (spec/filterer (if (keyword? arg)
-                         (fn [x]
-                           (get x arg))
-                         arg))
-
-        take
-        (spec/srange-dynamic (constantly 0) (fn [coll]
-                                              (min arg (count coll))))
-
-        drop
-        (spec/srange-dynamic (constantly arg)
-                             (fn [coll]
-                               (count coll)))
-
-        nil->val
-        (spec/nil->val arg)
-
-        (assert false (str "unrecognized method " (first path) path) )))
-
-    (contains? #{'ATOM 
-                 'ALL 
-                 'FIRST
-                 'LAST
-                 'MAP-VALS
-                 'META
-                 'END} path)
-    (assert false (str "unimplemented special path: " path))
-
-    :else
-    (assert false (str "unhandled path: " path)))
-  )
-
-
 
 
 (defn make-datascript-handler [conn query eid]
@@ -295,3 +182,4 @@
 ;;     (update-in test-val [:coord]
 ;;                assoc :lrow lrow :lcol lcol)
 ;;     test-val))
+
